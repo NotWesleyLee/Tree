2b123f0b604256beccce8eb26d31b7ed44c5a45b
shotun123 <60627668+shotun123@users.noreply.github.com>
2020-04-14 20:36:26 -0700

    Add files via upload

---
+++ utils.h
@@ -0,0 +1,20 @@
+//
+//  utila.h
+//  linked_list
+//
+//  Created by William McCarthy on 094//20.
+//  Copyright © 2020 William McCarthy. All rights reserved.
+//
+
+#ifndef utils_h
+#define utils_h
+
+#include <stdbool.h>
+
+const char* yesorno(bool condition)
+{
+	return condition ? "yes" : "no";
+}
+
+
+#endif /* utila_h */
